f49aa51b7cbc778353d4ffdfa4cefc502e06dd40
YEONJUN LEE <154724697+typhoon36@users.noreply.github.com>
2026-01-14 15:10:14 +0900

    feat :

---
+++ 0114/0114/Class2.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0114
+{ //부모클래스 : 기본 캐릭터
+//    class Character
+//    {
+//        //public ,private , protected  //상속이되어있는 자식이 사용가능하게 열어주는 접근제어자
+//        protected string name;
+//        protected int level;
+//        protected int hp;
+//        protected int maxHP;
+//        protected int attack;
+//        protected int defense;
+
+//        public Character(string characterName)
+//        {
+//            name = characterName;
+//            level = 1;
+//            maxHP = 100;
+//            hp = maxHP;
+//            attack = 30;
+//            defense = 20;
+
+//            Console.WriteLine($"캐릭터 {name} 생성!");
+//        }
+
+//        public virtual void ShowInfo()
+//        {
+//            Console.WriteLine($"이름 : {name}");
+//            Console.WriteLine($"레벨 : {level}");
+//            Console.WriteLine($"HP : {hp}/{maxHP}");
+//            Console.WriteLine($"공격력 : {attack}");
+//            Console.WriteLine($"방어력 : {defense}");
+//        }
+
+//    }
+
+//    //자식클래스 : 전사
+//    class Warrior : Character
+//    {
+//        private int rage; //전사만의 고유 속성
+
+//        public Warrior(string name) : base(name) //base로 부모 생성자 호출때 매개변수 전달
+//        {
+//            //name = "전사";  //base.name과 같다. 부모꺼 명시적으로 호출
+//            attack = 60;
+//            defense = 40;
+//            maxHP = 150;
+//            hp = maxHP;
+//            rage = 0;
+
+//            Console.WriteLine("직업 : 전사");
+//        }
+
+//        public override void ShowInfo()
+//        {
+//            base.ShowInfo(); //부모의 ShowInfo 호출
+//            Console.WriteLine($"분노 : {rage}");
+//        }
+//    }
+
+
+//    internal class Class2
+//    {
+//        static void Main(string[] args)
+//        {
+//            Character character = new Character("어쌔신");
+//            character.ShowInfo();
+//            // Warrior warrior = new Warrior("홍길동");
+
+
+
+//            //상속관계  
+//            //자식호출 ->   부 ->생성자  
+//            //             자  ->생성자
+//            //             자  ->소멸자
+//            //             부  ->소멸자
+
+
+
+//        }
+//    }
+}
---
+++ 0114/0114/Class3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0114
+{
+    public class Character
+    {
+        public virtual void Render()
+        {
+            Console.WriteLine("캐릭터");
+        }
+    }
+
+    public class Warrior : Character
+    {
+        public override void Render()
+        {
+            Console.WriteLine("워리어");
+        }
+    }
+
+    public class Mage : Warrior
+    {
+        public override void Render()
+        {
+            Console.WriteLine("마법사");
+        }
+    }
+
+
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            // Character character = new Character();
+            // character.Render(); //캐릭터
+            //Character character = new Warrior(); //업캐스팅   부모 <- 자식메모리 참조
+            //character.Render();
+            //Character character = new Mage();
+            //character.Render();//마법사
+
+            //Warrior warrior = new Mage();
+            //warrior.Render();
+
+            // Mage mage = new Mage();
+            // mage.Render();
+
+            Character character = new Warrior();  //업캐스팅
+
+            Warrior warrior = (Warrior)character; //다운캐스팅
+
+            warrior.Render();
+
+            ////is 연산자 문법
+            //if (character is Warrior)
+            //{
+            //    Warrior warrior = (Warrior)character;
+            //    warrior.Render();
+            //}
+
+            //Warrior warrior = character as Warrior;
+
+            //if (warrior != null)
+            //{
+            //    warrior.Render();
+            //}
+
+
+        }
+    }
+}
+
